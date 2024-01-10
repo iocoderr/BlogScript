8b0a3c964caa5dced76c22f0339f32f03e9b44ed
iocoderr <961112060@qq.com>
2024-01-10 12:23:38 +0800

    update BlogTool.py

--- BlogTool.py
+++ BlogTool.py
@@ -27,7 +27,7 @@ IconList={
     "home":"M6.906.664a1.749 1.749 0 0 1 2.187 0l5.25 4.2c.415.332.657.835.657 1.367v7.019A1.75 1.75 0 0 1 13.25 15h-3.5a.75.75 0 0 1-.75-.75V9H7v5.25a.75.75 0 0 1-.75.75h-3.5A1.75 1.75 0 0 1 1 13.25V6.23c0-.531.242-1.034.657-1.366l5.25-4.2Zm1.25 1.171a.25.25 0 0 0-.312 0l-5.25 4.2a.25.25 0 0 0-.094.196v7.019c0 .138.112.25.25.25H5.5V8.25a.75.75 0 0 1 .75-.75h3.5a.75.75 0 0 1 .75.75v5.25h2.75a.25.25 0 0 0 .25-.25V6.23a.25.25 0 0 0-.094-.195Z"
 }
 ######################################################################################
-class GMEEK():
+class BlogTool():
     def __init__(self,options):
         self.options=options
         
@@ -303,7 +303,7 @@ parser.add_argument("repo_name", help="repo_name")
 parser.add_argument("--issue_number", help="issue_number", default=0, required=False)
 options = parser.parse_args()
 
-blog=GMEEK(options)
+blog=BlogTool(options)
 
 if not os.path.exists("blogBase.json"):
     print("blogBase is not exists, runAll")
